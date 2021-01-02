89f8fe2d51a725acbb41c5bcad87ab693e044835
Peetu Kaan <aripeetu.kaan@gmail.com>
2021-01-02 12:16:34 +0000

    Refactored

--- SudokuVerifier/main.cpp
+++ SudokuVerifier/main.cpp
@@ -2,89 +2,98 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <sstream>
 #include <algorithm>
 
+static char sudoku_grid[9][9];
 
-std::vector<char> splitSolutionToChars(std::string possible_solution) {
-	std::vector<char> characters(possible_solution.begin(), possible_solution.end());
+std::vector<char> split(std::string solution) {
+	//Split the string
+	std::vector<char> characters(solution.begin(), solution.end());
 	return characters;
 }
 
-int verify(std::string solution) {
-    std::vector<char> characters = splitSolutionToChars(solution);
-    int k = 0;
-    char numbers[9][9];
-    for (int i = 0; i < 9; i++) {
-    	for (int j = 0; j < 9; j++) {
-    		numbers[i][j] = char(characters[k]);
-    		k++;
+void buildGrid(std::string solution) {
+	//Build the grid
+    std::vector<char> digits = split(solution);
+    int count = 0;
+    for (int line = 0; line < 9; line++) {
+    	for (int row = 0; row < 9; row++) {
+    		sudoku_grid[line][row] = char(digits[count]);
+    		count++;
     	}
     }
+}
 
-    //Testing the digits
-    int solution_length = solution.length();
-    if (solution_length != 81) {
-    	return -1;
-    }
-    else {
-    	bool positive_digit = (solution.find_first_not_of( "123456789" ) != std::string::npos);
-    		if (positive_digit == true) {
-    			//Incorrect digits
-    			return -1;
-    		}
-    }
+bool verifyDigits(std::string solution) {
+	//Verifying the digits. No characters or negative numbers allowed. Length of the solution has to be 81.
+	int solution_length = solution.length();
+	if (solution_length != 81) {
+	//Invalid length
+		return false;
+	}
+	else {
+		bool positive_digit = (solution.find_first_not_of( "123456789" ) != std::string::npos);
+	    if (positive_digit == true) {
+	    	//Incorrect digits
+	    	return false;
+	    }
+	}
+	return true;
+}
 
-    // Testing the lines
-    for (int i = 0; i < 9; i++) {
-    	std::vector<char> list;
-    	for (int j = 0; j < 9; j++) {
-    		bool found = std::find(list.begin(), list.end(), numbers[i][j]) != list.end();
-    		if (found == false) {
-    			char x = char(numbers[i][j]);
-    			list.push_back(x);
+int verifyLines(char sudoku_grid [9][9]) {
+	 for (int line = 0; line < 9; line++) {
+	    	std::vector<char> list_of_digits;
+	    	for (int row = 0; row < 9; row++) {
+	    		bool digit_found = std::find(list_of_digits.begin(), list_of_digits.end(), sudoku_grid[line][row]) != list_of_digits.end();
+	    		if (digit_found == false) {
+	    			//The line so far is valid
+	    			char new_digit = char(sudoku_grid[line][row]);
+	    			list_of_digits.push_back(new_digit);
+	    		}
+	    		else {
+	    		//Incorrect line
+	    			return false;
+	    		}
+	    	}
+	    }
+	 return true;
+}
+
+bool verifyRows(char sudoku_grid[9][9]) {
+    for (int row = 0; row < 9; row++) {
+    	std::vector<char> list_of_digits;
+    	for (int line = 0; line < 9; line++) {
+    		bool digit_found = std::find(list_of_digits.begin(), list_of_digits.end(), sudoku_grid[line][row]) != list_of_digits.end();
+    		if (digit_found == false) {
+    			//The row so far is valid
+    			char new_digit = char(sudoku_grid[line][row]);
+    			list_of_digits.push_back(new_digit);
     		}
     		else {
-    		//Incorrect line. Return -3.
-    			return -3;
+    		//Incorrect row
+    			return false;
     		}
     	}
     }
+    return true;
+}
 
-    // Testing the rows
-    for (int j = 0; j < 9; j++) {
-    	std::vector<char> list;
-    	for (int i = 0; i < 9; i++) {
-    		bool found = std::find(list.begin(), list.end(), numbers[i][j]) != list.end();
-    		if (found == false) {
-    			char x = char(numbers[i][j]);
-    			list.push_back(x);
-    		}
-    		else {
-    		//Incorrect row. Return -4.
-    			return -4;
-    		}
-    	}
-    }
-
-	// Testing the 3x3 sub-grids
-	int lineStart = 0;
-	int lineLimit = 3;
-	int rowStart = 0;
-	int rowLimit = 3;
-	for (int g = 0; g < 3; g++) {
-		for (int h = 0; h < 3; h++) {
+bool verifySubGrids(char sudoku_grid[9][9]) {
+	int lineStart = 0, lineLimit = 3, rowStart = 0, rowLimit = 3;
+	for (int line = 0; line < 3; line++) {
+		for (int row = 0; row < 3; row++) {
 			std::vector<char> list;
 			for (int i = lineStart; i < lineLimit; i++) {
 				for (int j = rowStart; j < rowLimit; j++) {
-			    	bool loytyyko = std::find(list.begin(), list.end(), numbers[i][j]) != list.end();
+			    	bool loytyyko = std::find(list.begin(), list.end(), sudoku_grid[i][j]) != list.end();
 					if (loytyyko == false) {
-						char x = char(numbers[i][j]);
+						char x = char(sudoku_grid[i][j]);
 						list.push_back(x);
 					}
 					else {
-						//Incorrect sub-grid. Return -2.
-						return -2;
+						//Incorrect sub-grid
+						return false;
 					}
 				}
 			}
@@ -96,12 +105,38 @@ int verify(std::string solution) {
 		rowStart = 0;
 		rowLimit = 3;
 	}
+	return true;
+}
+
+int verify(std::string solution) {
+    //Build grid (9x9)
+	buildGrid(solution);
+
+	//Verify Digits. If not valid return -1.
+	if (verifyDigits(solution) == false) {
+		return -1;
+	}
+
+    // Verify lines. If not valid return -3.
+    if (verifyLines(sudoku_grid) == false) {
+    	return -3;
+    }
+
+    // Verify rows. If not valid return -4.
+    if (verifyRows(sudoku_grid) == false) {
+    	return -4;
+    }
+
+	// Verify 3x3 sub-grids. If not valid return -2.
+    if (verifySubGrids(sudoku_grid) == false) {
+    	return -2;
+    }
 
 	// Valid solution. Return 0.
 	return 0;
 }
 
-int main() {
+void showInfo() {
     std::cout << "----SUDOKU VERIFIER----" << std::endl
     		<< "Valid solution:      0" << std::endl
 			<< "Invalid digits:     -1" << std::endl
@@ -109,20 +144,30 @@ int main() {
 			<< "Incorrect line:     -3" << std::endl
 			<< "Incorrect row:      -4" << std::endl
 			<< "(See help: verifier.pdf) \r" << std::endl;
+}
 
-    std::cout  << "--------RESULTS--------" << std::endl;
+void start() {
+	//Show info
+	showInfo();
+	//Open the file containing the solutions.
 	std::ifstream myfile;
 	myfile.open("solutions.txt");
 	std::string line;
 	int line_number = 0;
+	//Go through the file line by line. Verify solutions and show the results.
+    std::cout  << "--------RESULTS--------" << std::endl;
 	while (std::getline(myfile, line)) {
 		line_number++;
 		if (verify(line) == 0) {
 			std::cout << "Solution "<< line_number << ":          " <<  verify(line) << std::endl;;
 
 		} else {
-		std::cout << "Solution "<< line_number << ":         " <<  verify(line) << std::endl;;
+			std::cout << "Solution "<< line_number << ":         " <<  verify(line) << std::endl;;
 		}
 	}
+}
+
+int main() {
+	start();
     return 0;
 }
